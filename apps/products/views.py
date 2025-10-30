bd63657f9a6177e37091ebd2ef65f5592a9348af
Aroldo <aroldo.caval@gmail.com>
2025-10-30 00:16:06 -0300

    feat: criação do cadastro do produto

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import Product_Form, Product_Variant_Form
+
+class Product_Register_View(View):
+    def get(self, request):
+        context = {
+            'form_products': Product_Form(),
+            'form_variant': Product_Variant_Form(),
+        }
+        return render(request, 'products/register.html', context)
--- setup/settings.py
+++ setup/settings.py
@@ -41,9 +41,15 @@ INSTALLED_APPS = [
     "apps.stores",
     "apps.utils",
     # terceiros
+    "crispy_forms",
+    "crispy_bootstrap5",
     "cloudinary",
     "cloudinary_storage",
 ]
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ("bootstrap5",)
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -124,7 +130,7 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",                   
-    BASE_DIR / "apps" / "products" / "static",  
+    # BASE_DIR / "apps" / "products" / "static",  
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
