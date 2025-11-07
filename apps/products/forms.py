8e05991c7a91d6d19b6489402e672358c1ba7113
Aroldo <aroldo.caval@gmail.com>
2025-11-07 08:13:03 -0300

    feat: Criando forms de cadastro do produto

--- apps/products/forms.py
+++ apps/products/forms.py
@@ -11,19 +11,39 @@ class Product_Form(forms.Form):
         label="Descrição",
         widget=forms.Textarea(attrs={'rows': 4, 'placeholder': 'Descreva o produto...'})
     )
-    is_active = forms.BooleanField(
-        label="Ativo",
-        required=False,
+    image = forms.ImageField(
+        label="Imagem",
+        widget=forms.ClearableFileInput()
     )
 
-class Product_Variant_Form(forms.Form):
-    # skull = 
+class Product_Variant_Form(forms.Form): 
+    TYPE_CHOICES = [
+        ("DEMANDA", "Demanda"),
+        ("ESTOQUE", "Estoque"),
+    ]
+    sku = forms.CharField(max_length=50, required=False)
     description = forms.CharField(
         label="Descrição",
         widget=forms.Textarea(attrs={'rows': 4, 'placeholder': 'Descreva o produto...'})
     )
     price = forms.DecimalField(
         label="Preço",
-        min_value = 0,
+        min_value=0,
         decimal_places=2,
     )
+    variant_type = forms.ChoiceField(
+        label="Tipo", 
+        choices=TYPE_CHOICES,
+        widget=forms.RadioSelect()
+    )
+    stock = forms.IntegerField(
+        label="Estoque", 
+        required=False,
+        widget= forms.IntegerField(attrs={'placeholder': 'Quantidade em Estoque..'})
+    )
+    production_days = forms.IntegerField(label="Dias de Produção", required=False)
+    is_customizable = forms.BooleanField(label="Personalizável", required=False, initial=False)
+
+    class Atributos_Variant_Form(forms.Form):
+        atributo = forms.CharField(max_length=50, )
+        value = forms.CharField(max_length=50)
