7701205546c5044cb3bd2c84e6ea842af94c4250
Aroldo <aroldo.caval@gmail.com>
2025-11-14 13:11:38 -0300

    feat: criação do forms de Lojas

--- apps/stores/urls.py
+++ apps/stores/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', views.Store_Register_View.as_view(), name='register')
+]
--- apps/stores/views.py
+++ apps/stores/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import Store_Form, StoreCategories_Form
 
-# Create your views here.
+class Store_Register_View(View):
+    def get(self, request):
+        context = {
+            'form': Store_Form(),
+            'form_category': StoreCategories_Form()
+        }
+        return render(request, 'stores/register.html', context)
