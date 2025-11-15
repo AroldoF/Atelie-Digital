a60b48822f74ca62ac4917794d58540b1e350fdf
Alessandra <alessandraaquino2003@gmail.com>
2025-11-15 17:15:14 -0300

    feat: criando visualizacao inicial da tela de visualização do produto e componente de review

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,5 +5,6 @@ template_name = "products"
 
 urlpatterns = [
     path('register/', views.Product_Register_View.as_view(), name='register'),
-    path('searchProducts/', views.searchProduct, name='searchProduct')
+    path('searchProducts/', views.searchProduct, name='searchProduct'),
+    path('detailProducts/', views.detailProduct, name='detailProducts')
 ]
