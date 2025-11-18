a32a600cfcc93382abc58895c05baff81a6ee8b3
RenataEmy <renata.emy2012@gmail.com>
2025-11-17 22:31:08 -0300

    feat: adicionar estrutura da tela de checkout de pedidos

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("shopping_cart/", views.shopping_cart, name="shopping_cart")
+    path("shopping_cart/", views.shopping_cart, name="shopping_cart"),
+    path("order_checkout/", views.order_checkout, name="order_checkout")
 ]
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def shopping_cart(request):
     return render(request, 'orders/shopping_cart.html')
+
+def order_checkout(request):
+    return render(request, 'orders/order_checkout.html')
