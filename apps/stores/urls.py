1957df045f085725c188e3563548fd1c43c3c4b8
Aroldo <aroldo.caval@gmail.com>
2025-11-19 15:08:04 -0300

    fix: Resolução de bugs nos favoritos

--- apps/stores/urls.py
+++ apps/stores/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     path('register/', views.Store_Register_View.as_view(), name='register-store'),
      path('store-profile/', views.storeProfile, name='storeProfile'),
      path('dashboard/', views.dashboard, name='dashboard'),
      path('artisan-products/', views.artisan_products, name='artisan_products'),
