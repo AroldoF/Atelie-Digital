32490971e6ef8c22b15c1ddcc7323d949af45330
Aroldo <aroldo.caval@gmail.com>
2025-11-19 15:55:11 -0300

    fix: redirecionamentos do fluxo de compra

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("list/", views.list, name="list"),
     path("shopping_cart/", views.shopping_cart, name="shopping_cart"),
     path("checkout/", views.checkout, name="checkout"),
-    path("checkout/approved/", views.approved, name="approved"),
+    path("approved/", views.approved, name="approved"),
     path('confirm-address/', views.confirmAddress, name='confirm-address')
 ]
