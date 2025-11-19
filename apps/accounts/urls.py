3fbffc43c7eb856de770b05c9ebcd71a7d9aaa2f
RenataEmy <renata.emy2012@gmail.com>
2025-11-19 09:50:02 -0300

    fix: ajustar redirecionamento de parte das telas do accounts

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
-    path("settings/", views.settings, name="settings"),
     path("settings/user", views.settings_user, name="settings_user"),
     path("settings/address", views.settings_address, name="settings_address"),
     path("settings/artisian", views.settings_artisian, name="settings_artisian"),
