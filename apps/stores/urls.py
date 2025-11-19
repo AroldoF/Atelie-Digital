0cd2372a816c580b6ce5ae04f81329d653286b29
PKennedyP <kennedybad21@gmail.com>
2025-11-19 11:59:15 -0300

    feat: adiciona rotas para produtos e pedidos

--- apps/stores/urls.py
+++ apps/stores/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
      path('store-profile/', views.storeProfile, name='storeProfile'),
      path('dashboard/', views.dashboard, name='dashboard'),
-     path('artisan-products/', views.artisan_products, name='artisan_products')gi
+     path('artisan-products/', views.artisan_products, name='artisan_products'),
      path('artisan-orders/', views.artisan_orders, name='artisan_orders'),
 ]
