c0719f3fd4b69ccd1761992fa28c46b5c86b2c57
RenataEmy <renata.emy2012@gmail.com>
2026-01-16 14:42:08 -0300

    feat: incluir funcionalidade de adicionar ao carrinho

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from apps.accounts.models import Address
-from .models import Order
+from .models import Order, ProductVariant, Cart, CartItem
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView
+
+
+@require_POST 
+def addToCart(request):
+    # Captura os dados enviados pelo formulário do outro app
+    variant_id = request.POST.get('variant_id')
+    quantity = int(request.POST.get('quantity'))
+
+    # Busca a variante no banco (validação básica)
+    variant = get_object_or_404(ProductVariant, pk=variant_id)
+
+    # Criar/Pegar o carrinho
+    
+    cart_obj, created = Cart.objects.new_or_get(request)
+
+    # Criar/Pegar os itens do carrinho
+    item, created = CartItem.objects.get_or_create(cart=cart_obj, product_variant=variant, defaults={'quantity': quantity})
+    if not created:
+        item.quantity += quantity
+        item.save()
+
+    # 3. Redireciona o usuário 
+    return render(request, 'orders/shopping_cart.html') 
+
+def viewCart(request):
+    cart = Cart.objects.new_or_get(request)
+
+    items = cart.items.select_related('product_variant').all()
+
+    context = {'cart': cart, 'items': items}
+    
+    return render(request, 'orders/shopping_cart.html', context)
 
 def orders_detail(request, order_id):
     order = get_object_or_404(Order, pk=order_id)
@@ -13,10 +47,6 @@ def shipping(request):
     addresses = Address.objects.filter(user=request.user)
     context = {'addresses': addresses}
     return render(request, 'orders/shipping.html', context)
-
-@login_required
-def shopping_cart(request):
-    return render(request, 'orders/shopping_cart.html')
 
 def checkout(request):
     return render(request, 'orders/checkout.html')
