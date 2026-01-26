84905c1ca81bf11548cfddf64dabf6c95fe85354
Alessandra <alessandraaquino2003@gmail.com>
2026-01-26 12:56:05 -0300

    feat: template  e css das páginas de erro

--- setup/urls.py
+++ setup/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+
 from apps.core.views import (
     error_404_view,
     error_500_view,
